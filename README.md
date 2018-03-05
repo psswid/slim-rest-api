24c515e9a2a256a48065fc38b943082652f5defc
psswid <psswiderski@gmail.com>
2018-03-05 14:42:46 +0100

    ready working app

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # slim rest api
+
+PHP Rest API based on Traversy Media RESTful API With PHP & MySQL
+
+Writen OOP way.
+
+Technologies used:
+-MySQL,
+-php,
+-Slim 3.0
+
+This is my first attempt to write REST API in Slim php framework to get to know faster way to write REST API. Database contains 1 tables: categories, products. See database.json
+
+To see customers from database type /api/customers
+To see single customer type /api/customer/{id of customer}
+To add customer go to browser api addon, set url to public/api/customer/add and post and type
+
+{
+    "first_name": "aaa",
+    "last_name": "bbb",
+    "phone": "666 666 666 ",
+    "email": "asd@asd",
+    "city": "ASD",
+    "state": "DSA"
+}
+in body
+
+To update customer /api/customer/update/{id} and put same body as above with updated data
+To delete customer /api/customer/delete/{id}
---
+++ src/routes/customers.php
@@ -0,0 +1,165 @@
+<?php
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+
+$app = new \Slim\App;
+
+//Get all customers
+$app->get('/api/customers', function(Request $request, Response $response){
+  $sql = "SELECT * FROM customers";
+
+  try{
+    //get DB obj
+    $db = new Db();
+    //connect
+    $db = $db->connect();
+
+    $stmt = $db->query($sql);
+
+    $customers = $stmt->fetchAll(PDO::FETCH_OBJ);
+
+    $db = null;
+
+    echo json_encode($customers);
+  }catch(PDOException $e){
+    echo '{"error": {"text": '.$e->getMessage().'}}';
+  }
+});
+
+
+//Get single customer
+$app->get('/api/customer/{id}', function(Request $request, Response $response){
+
+  $id = $request->getAttribute('id');
+
+  $sql = "SELECT * FROM customers WHERE id = $id";
+
+  try{
+    //get DB obj
+    $db = new Db();
+    //connect
+    $db = $db->connect();
+
+    $stmt = $db->query($sql);
+
+    $customer = $stmt->fetchAll(PDO::FETCH_OBJ);
+
+    $db = null;
+
+    echo json_encode($customer);
+  }catch(PDOException $e){
+    echo '{"error": {"text": '.$e->getMessage().'}}';
+  }
+});
+
+
+//Add customer
+$app->post('/api/customer/add', function(Request $request, Response $response){
+
+  $first_name = $request->getParam('first_name');
+  $last_name = $request->getParam('last_name');
+  $phone = $request->getParam('phone');
+  $email = $request->getParam('email');
+  $city = $request->getParam('city');
+  $state = $request->getParam('state');
+
+
+
+  $sql = "INSERT INTO customers (first_name, last_name, phone, email, city, state) VALUES(:first_name, :last_name, :phone, :email, :city, :state)";
+
+  try{
+    //get DB obj
+    $db = new Db();
+    //connect
+    $db = $db->connect();
+
+    $stmt = $db->prepare($sql);
+    $stmt->bindParam(':first_name', $first_name);
+    $stmt->bindParam(':last_name', $last_name);
+    $stmt->bindParam(':phone', $phone);
+    $stmt->bindParam(':email', $email);
+    $stmt->bindParam(':city', $city);
+    $stmt->bindParam(':state', $state);
+
+    $stmt->execute();
+
+    echo '{"notice": {"text": "customer added"}}';
+
+  }catch(PDOException $e){
+    echo '{"error": {"text": '.$e->getMessage().'}}';
+  }
+});
+
+
+//Update customer
+$app->put('/api/customer/update/{id}', function(Request $request, Response $response){
+
+  $id = $request->getAttribute('id');
+  $first_name = $request->getParam('first_name');
+  $last_name = $request->getParam('last_name');
+  $phone = $request->getParam('phone');
+  $email = $request->getParam('email');
+  $city = $request->getParam('city');
+  $state = $request->getParam('state');
+
+
+
+  $sql = "UPDATE customers SET
+          first_name = :first_name,
+          last_name = :last_name,
+          phone = :phone,
+          email = :email,
+          city = :city,
+          state = :state
+          WHERE id = $id";
+
+  try{
+    //get DB obj
+    $db = new Db();
+    //connect
+    $db = $db->connect();
+
+    $stmt = $db->prepare($sql);
+    $stmt->bindParam(':first_name', $first_name);
+    $stmt->bindParam(':last_name', $last_name);
+    $stmt->bindParam(':phone', $phone);
+    $stmt->bindParam(':email', $email);
+    $stmt->bindParam(':city', $city);
+    $stmt->bindParam(':state', $state);
+
+    $stmt->execute();
+
+    echo '{"notice": {"text": "customer updated"}}';
+
+  }catch(PDOException $e){
+    echo '{"error": {"text": '.$e->getMessage().'}}';
+  }
+});
+
+//Delete customer
+$app->delete('/api/customer/delete/{id}', function(Request $request, Response $response){
+
+  $id = $request->getAttribute('id');
+
+
+  $sql = "DELETE FROM customers
+          WHERE id = $id";
+
+  try{
+    //get DB obj
+    $db = new Db();
+    //connect
+    $db = $db->connect();
+
+    $stmt = $db->prepare($sql);
+
+    $stmt->execute();
+    $db=null;
+
+    echo '{"notice": {"text": "customer deleted"}}';
+
+  }catch(PDOException $e){
+    echo '{"error": {"text": '.$e->getMessage().'}}';
+  }
+});
